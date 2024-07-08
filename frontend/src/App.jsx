31fa9c46b5aa7ff7fc7a3e9d11ffe267721bbf75
vaibhav ojha <vaibhavojha34@gmail.com>
2024-07-08 17:18:24 +0530

    final commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,29 @@
 import Home from "./home/Home";
 import React from "react";
-import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Course from "./components/Course";
-import Footer from "./components/Footer";
+import { Route, Routes, Navigate } from "react-router-dom";
+
 import Courses from "./courses/Courses";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Contact from "./components/Contact";
-import  { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
 
-
-
-
 function App() {
-  const [authUser,setAuthUser] = useAuth();
+  const [authUser, setAuthUser] = useAuth();
   return (
     <div>
       {/* <Home/> */}
-      <div className=' dark:bg-slate-900 dark:text-white'>
+      <div className=" dark:bg-slate-900 dark:text-white">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/course" element={authUser?<Courses/>:<Navigate to = "/signup"/>} />
-          <Route path="/login" element = {<Login/>}/>
-          <Route path="/signup" element = {<Signup/>}/>
-          <Route path="/contact" element = {<Contact/>}/>
-          
-          
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
       <Toaster />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode:"class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    fontFamily: {
-      
-      
-     
-    },
+    fontFamily: {},
   },
   plugins: [require("daisyui")],
-  daisyui:{
-    themes:["light"]
-  }
-}
+  daisyui: {
+    themes: ["light"],
+  },
+};
